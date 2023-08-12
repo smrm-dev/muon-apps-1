6d70c18d0738accd8bd290b14744afc4c0159936
smrm-dev <smrmdev@gmail.com>
2023-08-12 11:28:08 +0330

    save

--- general/muon_adaptor.js
+++ general/muon_adaptor.js
@@ -1,9 +1,62 @@
-// const { soliditySha3 } = MuonAppUtils
+const { Web3 } = MuonAppUtils
+
+const CHAINS = {
+    mainnet: 1,
+    fantom: 250,
+    polygon: 137,
+    bsc: 56,
+    avax: 43114,
+}
+
+const networksWeb3 = {
+    [CHAINS.mainnet]: new Web3("https://rpc.ankr.com/eth"),
+    [CHAINS.fantom]: new Web3("https://rpc.ankr.com/fantom"),
+    [CHAINS.polygon]: new Web3("https://rpc.ankr.com/polygon"),
+    [CHAINS.bsc]: new Web3("https://rpc.ankr.com/bsc"),
+    [CHAINS.avax]: new Web3("https://rpc.ankr.com/avalanche"),
+}
 
 const MuonAdaptorApp = {
     APP_NAME: 'muon_adaptor',
 
-    getBlocksHashes: async function (chainId, blockNumbers) { },
+    makeEthGetBlockRequest: function (id, blockNumber) {
+        return {
+            jsonrpc: '2.0',
+            id,
+            method: 'eth_getBlockByNumber',
+            params: ['0x' + blockNumber.toString(16), false]
+        }
+
+    },
+
+    makeBatchRequest: async function (w3, requests) {
+        let batch = new w3.BatchRequest();
+
+        requests.forEach((request) => batch.add(request.req))
+        const responses = await batch.execute()
+
+        let results = new Array(requests.length)
+        for (let res of responses) {
+            results[res.id] = requests[res.id].decoder(res.result)
+        }
+
+        return results
+    },
+
+    getBlocksHashes: async function (chainId, blockNumbers) {
+        let requests = []
+        blockNumbers.forEach(
+            (index, blockNumber) => {
+                requests.push({
+                    req: this.makeEthGetBlockRequest(blockNumber, index),
+                    decoder: (res => res.hash),
+                })
+            },
+        )
+        const w3 = networksWeb3[chainId]
+        const hashes = await this.makeBatchRequest(w3, requests)
+        return hashes
+    },
 
     onRequest: async function (request) {
         let { method, data: { params } } = request;
